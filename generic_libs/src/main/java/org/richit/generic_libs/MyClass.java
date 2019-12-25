dfb3395d5dd18140119f25f558c66094e9e57a65
Md Rayhan <mdrayhan9464@gmail.com>
2019-12-26 00:41:43 +0600

    hashmap added

--- generic_libs/src/main/java/org/richit/generic_libs/MyClass.java
+++ generic_libs/src/main/java/org/richit/generic_libs/MyClass.java
@@ -1,6 +1,7 @@
 package org.richit.generic_libs;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class MyClass {
 
@@ -23,6 +24,17 @@ public class MyClass {
     public static void main(String[] args) {
 
         startProgramme();
+        HashMap<Integer, String> hashMaps = new HashMap<>();
+        hashMaps.put( 80, "Monjura" );
+        hashMaps.put( 60, "Jamal" );
+        hashMaps.put( 50, "Momena" );
+        hashMaps.put( 25, "Rayhan" );
+        hashMaps.put( 22, "Marjana" );
+        hashMaps.put( 17, "Farhan" );
+        hashMaps.put( 13, "Jihan" );
+        System.out.println(hashMaps);
+        System.out.println(hashMaps.values());
+        System.out.println(hashMaps.get( 25 ));
         ArrayList<String> strings = new ArrayList<>();
         strings.add( "Zero" );
         strings.add( "One" );
